402b0ab0950bfef7d033010930c79c3903d2a6d7
Benedict Newton <bendev2024@gmail.com>
2025-01-25 11:59:58 -0700

    changes to how it works

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const btn = document.getElementById('modeswitch');
-const mode = document.querySelectorAll('.lightdark')
+const mode = document.querySelectorAll('.light')
 
 function colorScheme() {
     mode.forEach(element => {
-        if (element.style.colorScheme === 'dark') {
-            element.style.colorScheme = 'light';
+        if (element.classList.contains('dark')) {
+            element.classList.remove('dark');
+            element.classList.add('light');
         } else {
-            element.style.colorScheme = 'dark';
+            element.classList.remove('light');
+            element.classList.add('dark');
         }
     });
 }
