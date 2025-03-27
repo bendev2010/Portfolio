d56d5bbecdcc82401620b4f6fd690e8fdf960f31
Benedict Newton <bendev2024@gmail.com>
2025-03-26 19:14:52 -0600

    changes and combination of neu

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const css = document.getElementById('css');
 const mobileNav = document.getElementById('mobileNav');
 
+
 function changeGlass() {
     css.href = 'css/glass.css';
-}
-
-function changeNeu() {
-    css.href = 'css/neu.css';
 }
