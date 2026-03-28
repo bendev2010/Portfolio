116f70175f4385a454552c1e29358e4b8655f0f0
Benedict Newton <bendev2024@gmail.com>
2026-03-28 14:22:22 -0600

    brilliant first step

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const css = document.getElementById('css');
-const mobileNav = document.getElementById('mobileNav');
-
+const glowCursor = document.querySelector('.cursor-glow');
 
 function changeGlass() {
     css.href = 'css/glass.css';
 }
+
+function toggleTheme() {
+    if (css.href.includes('glass')) {
+        css.href = 'css/styles.css';
+    } else {
+        css.href = 'css/glasses.css';
+    }
+}
+
+window.addEventListener('mousemove', (e) => {
+  const { clientX: x, clientY: y } = e;
+  glowCursor.style.left = x + 'px';
+  glowCursor.style.top = y + 'px';
+});
